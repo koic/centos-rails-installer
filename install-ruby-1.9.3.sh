1336cdff50976cf3887551edb687f3cb050fe8af
ITO Koichi <koic.ito@gmail.com>
2012-07-04 10:12:18 +0900

    Dont use rbenv

--- install-ruby-1.9.3.sh
+++ install-ruby-1.9.3.sh
@@ -23,7 +23,8 @@ wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
 tar zxvf ruby-1.9.3-p194.tar.gz
 cd ruby-1.9.3-p194
 
-./configure --prefix=$HOME/.rbenv/versions/1.9.3-p194
+./configure
+# ./configure --prefix=$HOME/.rbenv/versions/1.9.3-p194
 make
 make install
 
